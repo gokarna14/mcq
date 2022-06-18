486e3b808e3407024ff6781132dbe319770bbc50
System Administrator <root@Prabass-MacBook-Pro.local>
2022-06-18 14:12:24 +0545

    Added sort feature in the my progress table

--- src/js/exam/MyProgress.js
+++ src/js/exam/MyProgress.js
@@ -16,9 +16,9 @@ const MyProgress = (props)=>{
     const [score, setScore] = useState(0)
     const [numOfQuestions, setNumOfQuestions] = useState(0)
     const [showFullProgress, setShowFullProgress] = useState(false)
+    const [nowSorted, setNowSorted] = useState('ts')
 
-    const sortSign =   
-    <>
+    const sortSign =   <>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-up" viewBox="0 0 16 16">
             <path d="M3.5 12.5a.5.5 0 0 1-1 0V3.707L1.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.498.498 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 3.707V12.5zm3.5-9a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z"/>
         </svg>
@@ -32,6 +32,17 @@ const MyProgress = (props)=>{
         'quick': 8
     }
 
+    const addFullMarkAndCorrect=(arrayOfRecord)=>{
+        var temp = []
+        for (var examRecord of arrayOfRecord){
+            examRecord['fullMark'] = fullMarks[examRecord.response_type]
+            examRecord['correct%'] =  (examRecord.score/fullMarks[examRecord.response_type])*100
+            temp.push(examRecord)
+        }
+        // console.log(temp);
+        setUserExamRecords(temp);
+    }
+
     const refresh=()=>{
         if (props.examProps.universalProps.userLoggedIn){
             console.log("On PROGRESS");
@@ -39,21 +50,19 @@ const MyProgress = (props)=>{
                 userInf: props.examProps.universalProps.userInf
             }).then(
                 res=>{
-                    // console.log(res.data);
-                    setUserExamRecords(res.data);
+                    addFullMarkAndCorrect(res.data);
                 }
             )
         }
     }
 
     const viewFullProgress=(response_id, numOfQns)=>{
-        console.log(props);
+        // console.log(props);
         axios.post('/api/getResponseAndScore', {
             userInf: props.universalProps.userInfuserInf,
             all_response_id: response_id
         }).then(
             response=>{
-                // console.log(response.data); // todo: generate result
                 setFullResponse(response.data.response);
                 return response.data.score;
             }
@@ -70,22 +79,10 @@ const MyProgress = (props)=>{
 
 
     useEffect(()=>{
-        if (props.examProps.universalProps.userLoggedIn){
-            console.log("On PROGRESS");
-            axios.post('/api/userExamRecords', {
-                userInf: props.examProps.universalProps.userInf
-            }).then(
-                res=>{
-                    console.log(res.data);
-                    setUserExamRecords(res.data);
-                }
-            )
-        }
-        else{
-            // console.log("Not Logged In");
-        }
+        refresh();
         setShowLoading(false);
     }, [])
+
 
     const loopThroughExamRecords = userExamRecords.map(
         examRecord=>{
@@ -113,25 +110,44 @@ const MyProgress = (props)=>{
         }
     )
 
-    const sort=(what)=>{
 
+    const sortBy=async(col)=>{
+        if (userExamRecords.length > 0){
+            // console.log(`Sort by: ${col}`);
+            // console.log(userExamRecords);
+            var temp = userExamRecords.sort(
+                (first, second)=> (
+                    nowSorted === col ? 
+                    (second[col]>first[col] ? 1 :((first[col]>second[col] ? -1 : 0)))
+                    : 
+                    (first[col]>second[col]? 1 :((second[col]>first[col] ? -1 : 0)))
+                ))
+            setUserExamRecords(temp);
+            setScore(1000);
+            setNowSorted(nowSorted === col ? '' : col)
+        }
     }
 
     const allExamRecords=( userExamRecords.length > 0 ?
     <table className="table">
         <thead>
             <tr>
-                <th>Exam Type <span>{sortSign}</span>
+                <th>Exam Type <span
+                    onClick={()=>{sortBy('response_type')}}
+                >{sortSign}</span>
                 </th>
-                <th>Date <span>{sortSign}</span>
+                <th>Date <span
+                    onClick={()=>{sortBy('ts')}}>{sortSign}</span>
                 </th>
-                <th>Time <span>{sortSign}</span>
+                <th>Time</th>
+                <th>Score <span
+                    onClick={()=>{sortBy('score')}}>{sortSign}</span>
                 </th>
-                <th>Score <span>{sortSign}</span>
+                <th>Full Mark <span
+                    onClick={()=>{sortBy('fullMark')}}>{sortSign}</span>
                 </th>
-                <th>Full Mark <span>{sortSign}</span>
-                </th>
-                <th>Correct <span>{sortSign}</span>
+                <th>Correct <span
+                    onClick={()=>{sortBy('correct%')}}>{sortSign}</span>
                 </th>
                 <th>See Full Record</th>
             </tr>
